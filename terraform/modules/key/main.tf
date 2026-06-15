12a888aa32cfa6677f8fa651e31f75d6d8fc2201
Prabin R <prabindhas32@gmail.com>
2026-06-15 13:02:15 +0530

    "Updated the key file"

--- terraform/modules/key/main.tf
+++ terraform/modules/key/main.tf
@@ -3,6 +3,11 @@
   rsa_bits  = 4096
 }
 
+resource "local_file" "private_key" {
+  content  = tls_private_key.this.private_key_pem
+  filename = "prabin-terraform-key.pem"
+}
+
 resource "aws_key_pair" "this" {
   key_name   = var.key_name
   public_key = tls_private_key.this.public_key_openssh
