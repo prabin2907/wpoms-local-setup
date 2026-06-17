81dfd09e7ef39329cdc1df167c23ff48c58aaba8
Prabin R <prabindhas32@gmail.com>
2026-06-17 14:50:32 +0530

    changed the deploy.sh

--- scripts/upload-s3.sh
+++ scripts/upload-s3.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-# Replace placeholders with actual values
+echo "Generating docker-compose.yml with version v1.0.$BUILD_NUMBER..."
+
 sed -e "s|__ECR_REGISTRY__|$ECR_REGISTRY|g" \
     -e "s|__IMAGE_TAG__|v1.0.$BUILD_NUMBER|g" \
     docker-compose.ec2.yml > docker-compose.yml
 
 echo "Uploading docker-compose.yml to S3..."
 aws s3 cp docker-compose.yml s3://$S3_BUCKET/docker-compose.yml
+
+echo "Uploading deploy script to S3..."
+aws s3 cp scripts/deploy.sh s3://$S3_BUCKET/deploy.sh
 
 echo "Upload complete!"
