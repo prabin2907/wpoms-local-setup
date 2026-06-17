6d7d66bf6dc4630bff6bf8220565af3d0b7fbcf3
Prabin R <prabindhas32@gmail.com>
2026-06-17 15:19:09 +0530

    ECR VReistry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+ECR_REGISTRY="924982554051.dkr.ecr.ap-south-1.amazonaws.com"
+
 echo "Downloading docker-compose.yml from S3..."
 aws s3 cp s3://$S3_BUCKET/docker-compose.yml /home/ubuntu/app/docker-compose.yml
 
